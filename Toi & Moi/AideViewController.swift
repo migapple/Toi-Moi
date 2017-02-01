e09bb802124920e908e4d1e2d7b40b1a26013154
migapple <michel.garlandat@free.fr>
2017-02-01 17:35:31 +0100

    Ajout de l'aide

---
+++ Toi & Moi/AideViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AideViewController.swift
+//  Toi & Moi
+//
+//  Created by Michel Garlandat on 01/02/2017.
+//  Copyright © 2017 Michel Garlandat. All rights reserved.
+//
+
+import UIKit
+
+class AideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
